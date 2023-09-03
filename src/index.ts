36a2f50c2ba99ed0c50a9d9e1874504e60502034
Javier A Velazquez <javh.accs@gmail.com>
2023-09-03 12:09:51 -0400

    Add initial dependencies
    Add tailwindcss for CSS, typed-html for JSX templating

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
+import { staticPlugin } from '@elysiajs/static';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(staticPlugin());
+
+app.get('/', () => {
+  return new Response('ay santo1', {
+    headers: {
+      'content-type': 'text/html; charset=utf-8',
+    },
+  });
+});
+
+app.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
